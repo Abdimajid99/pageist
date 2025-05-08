d3e9b659494d7a26e905fc36e130e3c29e83906d
Abdimajid Shire <Abdulmjeedshire3@gmail.com>
2025-05-08 09:15:12 +0300

    feat: Add authService.ts to use Firebase auth functions

---
+++ src/services/authService.ts
@@ -0,0 +1,43 @@
+import { auth, googleProvider } from "../configs/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+export const signUpWithEmail = async (email: string, password: string) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to sign up with email and password");
+  }
+};
+
+export const signInWithEmail = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to sign in with email and password");
+  }
+};
+
+export const signInWithGoogle = async () => {
+  try {
+    await signInWithPopup(auth, googleProvider);
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to sign in with google auth provider");
+  }
+};
+
+export const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error(error);
+    throw new Error("failed to log out");
+  }
+};
